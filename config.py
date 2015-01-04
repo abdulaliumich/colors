84b3dea4a66ae199dd39d7d646083d6bf1cf23d3
abdulaliumich <abdulali@umich.edu>
2015-01-04 15:31:01 -0500

    added Create Account to navigation bar

--- config.py
+++ config.py
@@ -3,3 +3,13 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 SQLALCHEMY_DATABASE_URI = 'mysql://root@localhost/colors_website'
 SECRET_KEY = 'fjdssdgsdugpao'
+
+
+# mail server settings
+MAIL_SERVER = 'localhost'
+MAIL_PORT = 25
+MAIL_USERNAME = None
+MAIL_PASSWORD = None
+
+# administrator list
+ADMINS = ['abdulali@umich.edu']
