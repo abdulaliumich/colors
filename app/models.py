f78a6aabcb63c91cf55f6dfe15ae0c776ddf4a5c
abdulaliumich <abdulali@umich.edu>
2015-01-03 20:57:09 -0500

    added login page and user-color relationship table

--- app/models.py
+++ app/models.py
@@ -1,4 +1,11 @@
 from app import db
+from sqlalchemy import Table, Column, Integer, ForeignKey
+from sqlalchemy.orm import relationship, backref
+
+UserColors = db.Table ('UserColors',
+             db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
+             db.Column('color_name', db.String(30), db.ForeignKey('color.color_name'))
+)
 
 class Color(db.Model):
     color_name = db.Column(db.String(30), primary_key = True)
@@ -8,3 +15,17 @@ class Color(db.Model):
     
     def __repr__(self):
         return '<Color %r>' % self.color_name
+
+class User(db.Model):
+    user_id = db.Column(db.Integer, primary_key = True)
+    email = db.Column(db.String(60), unique = True)
+    username = db.Column(db.String(40), unique = True)
+    colors = relationship("Color", secondary=UserColors, backref="user")    
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+    
--- app/views.py
+++ app/views.py
@@ -12,6 +12,10 @@ def colors():
     colors = models.Color.query.all()
     return render_template('colors.html', title='Colors', colors=colors)
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
 @lm.user_loader
 def load_user(id):
     return User.query.get(int(id))
